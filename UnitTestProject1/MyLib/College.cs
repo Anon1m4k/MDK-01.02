48f50b7210f49b3a74faa234586120d966ccf41f
Anon1m4k <kirilvalera2@gmail.com>
2025-03-04 12:20:42 +0300

    тест

---
+++ UnitTestProject1/MyLib/College.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLib
+{
+    public class College
+    {
+        public Dictionary<string, List<string> = new List>
+        public void AddGroup(string group, )
+
+        public List<string> GetStudentsByGroup(string group) 
+        {
+         return new List<string>() { };
+        }
+    }
+}
