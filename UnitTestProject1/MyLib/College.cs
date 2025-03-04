4acd675a6f913cde2a4297be1da85daa30658340
Anon1m4k <kirilvalera2@gmail.com>
2025-03-04 13:14:34 +0300

    gege

--- UnitTestProject1/MyLib/College.cs
+++ UnitTestProject1/MyLib/College.cs
@@ -8,11 +8,15 @@ namespace MyLib
 {
     public class College
     {
-        public Dictionary<string, List<string> = new List>
-        public void AddGroup(string group, )
+        public Dictionary<string, List<string>> Students = new Dictionary<string, List<string>>();
+        public void AddGroup(string group, List <string> Student  )
+        {
+            Students.Add( group, Student );
+        }
 
         public List<string> GetStudentsByGroup(string group) 
         {
+          
          return new List<string>() { };
         }
     }
