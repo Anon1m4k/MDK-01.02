e4ecda0edcad9772d79b0b89f6ea0868e0182c85
Anon1m4k <kirilvalera2@gmail.com>
2025-03-13 13:14:04 +0300

    почемуто методы работают

--- CollegeTest/CollegeLib/College.cs
+++ CollegeTest/CollegeLib/College.cs
@@ -1,4 +1,5 @@
 ﻿
+using System;
 using System.Collections.Generic;
 
 namespace CollegeLib
@@ -8,11 +9,37 @@ namespace CollegeLib
         public Dictionary<string, List<string>> Students = new Dictionary<string, List<string>>();
         public void AddGroup(string group, List<string> Student)
         {
-            Students.Add(group, Student);
+            if (!Students.ContainsKey(group))
+            {
+                Students.Add(group, Student);
+            }
+            else
+            {
+                // Если группа уже существует, можно добавить студентов в существующую группу
+                Students[group].AddRange(Student);
+            }
+        }
+        public void AddStudentToGroup(string group, string Student)
+        {
+            if (Students.ContainsKey(group))
+            {
+                Students[group].Add(Student);
+            }
+            else
+            {
+                throw new Exception($"Группа '{group}' не найдена.");
+            }
         }
         public List<string> GetStudentsByGroup(string group)
         {
-            return new List<string>() { };
+            if (Students.ContainsKey(group))
+            {
+                return Students[group]; // Возвращаем список студентов, если группа найдена
+            }
+            else
+            {
+                return new List<string>(); // Возвращаем пустой список, если группа не найдена
+            }
         }
     }
 }
--- CollegeTest/CollegeTest/TCollege.cs
+++ CollegeTest/CollegeTest/TCollege.cs
@@ -11,12 +11,12 @@ namespace CollegeTest
     {
         Dictionary<string, List<string>> expectedInfo = new Dictionary<string, List<string>>()
         {
-            {"П-10", new List<string>() { "Баранов Даниил" , "Курчаков Артем", "Колобанов Захар"} },
-            {"Л-20", new List<string>() { "Смирнова Екатерина" , "Бычков Дмитрий", "Суханов Дмитрий"} },
-            {"Н-30", new List<string>() { "Венедиктов Дмитрий" , "Сучков Артем", "Водянов Захар"} },
+            //{"П-10", new List<string>() { "Баранов Даниил" , "Курчаков Артем", "Колобанов Захар"} },
+            //{"Л-20", new List<string>() { "Смирнова Екатерина" , "Бычков Дмитрий", "Суханов Дмитрий"} },
+            //{"Н-30", new List<string>() { "Венедиктов Дмитрий" , "Сучков Артем", "Водянов Захар"} },
         };
 
-        [TestMethod]
+        [TestMethod] //наличие любой группы
         public void TGetStudentsByGroupX()
         {
             College college = new College();
@@ -30,6 +30,71 @@ namespace CollegeTest
 
                 CollectionAssert.AreEqual(expected, actual);
             }
+        }
+
+        [TestMethod] //наличие любой группы
+        public void SomeGroup()
+        {
+            College college = new College();
+
+            // Добавляем группы
+            foreach (string group in expectedInfo.Keys)
+            {
+                college.AddGroup(group, expectedInfo[group]);
+            }
+
+            // Проверяем наличие групп
+            foreach (string group in expectedInfo.Keys)
+            {
+                List<string> actual = college.GetStudentsByGroup(group);
+                Assert.IsNotNull(actual, $"Группа '{group}' не найдена.");
+                Assert.IsTrue(actual.Count > 0, $"Группа '{group}' не содержит студентов.");
+            }
+        }
+
+        /*[TestMethod]
+        public void TGetStudentsByGroupX() //наличие любой группы
+        {
+            College college = new College();
+
+            // Проходим по всем группам в expectedInfo
+            foreach (string group in expectedInfo.Keys)
+            {
+                // Добавляем группу и студентов в колледж
+                college.AddGroup(group, expectedInfo[group]);
+
+                // Получаем фактический список студентов по группе
+                List<string> actual = college.GetStudentsByGroup(group);
+
+                // Создаем ожидаемый список студентов из словаря
+                List<string> expected = new List<string>(expectedInfo[group]);
+
+                // Сравниваем ожидаемый и фактический списки студентов
+                CollectionAssert.AreEqual(expected, actual, $"Список студентов для группы '{group}' не совпадает.");
+            }
+        }*/
+
+        [TestMethod] //наличие конкретной группы
+        public void AGroup()
+        {
+            College college = new College();
+
+            // Определяем группу и студентов
+            string groupName = "П-10";
+            List<string> students = new List<string>() { "Баранов Даниил", "Курчаков Артем", "Колобанов Захар" };
+
+            // Добавляем группу в колледж
+            college.AddGroup(groupName, students);
+
+            // Проверяем, что группа существует
+            List<string> actualStudents = college.GetStudentsByGroup(groupName);
+
+            // Проверяем, что список студентов не пустой
+            Assert.IsNotNull(actualStudents, $"Группа '{groupName}' не найдена.");
+            Assert.IsTrue(actualStudents.Count > 0, $"Группа '{groupName}' не содержит студентов.");
+
+            // Проверяем, что список студентов соответствует ожидаемому
+            CollectionAssert.AreEqual(students, actualStudents, $"Список студентов для группы '{groupName}' не совпадает.");
         }
     }
 }
